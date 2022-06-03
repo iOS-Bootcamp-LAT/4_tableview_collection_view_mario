364d8c7d2fd5dbeb520f0f1a3cc013bf3819907c
David Granado J <david.granado@interfase.com.bo>
2022-06-03 09:21:14 -0400

    Initial Commit

---
+++ 4_tableview_collection_view_mario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4_tableview_collection_view_mario
+//
+//  Created by David Granado Jordan on 6/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
